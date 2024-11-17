04ce589173eb2c4b0dfdadd041d0c90e7d1805ca
Pavan Kothawade <pavan@prosimo.io>
2024-11-16 20:45:18 -0800

    section component with props

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ function Header() {
+   return (
+     <div>
+       header asd
+     </div>
+   )
+ }
+ 
+ export default Header
+ 
---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        RightBtnText="Exisitng Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        RightBtnText="Exisitng Inventory"
+      />
+     <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        RightBtnText="Exisitng Inventory"
+      />
+    </Container>
+  )
+}
+
+export default Home
+
+const Container = styled.div`
+    height: 100vh;
+`
